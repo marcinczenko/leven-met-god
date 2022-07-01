d60b522f1b1b7fc6191e701934fb10afa6cdc350
Marcin Czenko <marcin.czenko@pm.me>
2022-07-01 06:06:58 +0200

    226: WOENSDAG NA DE 2e ZONDAG

--- src/app-data/meditations.js
+++ src/app-data/meditations.js
@@ -962,5 +962,9 @@ export const meditations = {
   '2022-6-29T00:00:00': {
     title: '226: WOENSDAG NA DE 3e ZONDAG - Vertrouwen op Jezus Christus',
     link: '/226'
+  },
+  '2022-6-30T00:00:00': {
+    title: '227: DONDERDAG NA DE 3e ZONDAG - De aardse dingen',
+    link: '/227'
   }
 }
