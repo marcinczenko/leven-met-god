ccf1e79aaa275ee54a65a69f4f36976b53db134f
Marcin Czenko <marcin.czenko@pm.me>
2022-07-05 16:30:22 +0200

    Updates the <<deploy>> script to accept remote host name as an argument

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
+DEPLOYHOST="idbox-1-new.local"
+if [ -n "$1" ]; then
+  DEPLOYHOST=$1
+else
+  echo "No deployment hostname provided."
+fi
+
+echo "Deploying to $DEPLOYHOST"
+
 yarn build
 tar czvf public-lmg.tar.gz public/
-scp public-lmg.tar.gz pi@idbox-1-new.local:~/identity-box/deployments/
-ssh pi@idbox-1-new.local /home/pi/identity-box/deploy-lmg.sh
+scp public-lmg.tar.gz pi@$DEPLOYHOST:~/identity-box/deployments/
+ssh pi@$DEPLOYHOST /home/pi/identity-box/deploy-lmg.sh
 rm public-lmg.tar.gz
