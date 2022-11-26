b12247c34c8a4de04cd64267fc5f4cf16af5cad2
Marcin Czenko <marcin.czenko@pm.me>
2022-11-26 23:10:25 +0100

    Updates linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,39 @@
 module.exports = {
-  parser: '@babel/eslint-parser',
+  parser: '@typescript-eslint/parser',
   globals: {
-    __PATH_PREFIX__: true
+    __PATH_PREFIX__: true,
   },
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   settings: {
     react: {
-      version: 'detect'
-    }
+      version: 'detect',
+    },
   },
   extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
     'standard',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime'
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'prettier',
   ],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
-    },
-    babelOptions: {
-      presets: ['@babel/preset-react']
+      jsx: true,
     },
     requireConfigFile: false,
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ['@typescript-eslint', 'react', 'jest'],
   rules: {
-    'react/prop-types': 0
-  }
+    'react/prop-types': 0,
+  },
 }
