fdfc90b8ded6ba5b94b7a017d039a21f22601152
Marcin Czenko <marcin.czenko@pm.me>
2022-11-28 04:09:00 +0100

    fixes dates

--- src/app-data/meditations.js
+++ src/app-data/meditations.js
@@ -3,35 +3,35 @@ export const meditations = {
     title: '376: ZATERDAG NA DE 24e ZONDAG - Het licht van de wereld',
     link: '/376'
   },
-  '2021-11-27T00:00:00': {
+  '2022-11-27T00:00:00': {
     title: '1: 1e ZONDAG VAN DE ADVENT - De komst des Heren',
     link: '/1'
   },
-  '2021-11-28T00:00:00': {
+  '2022-11-28T00:00:00': {
     title: '2: MAANDAG NA DE 1e ZONDAG - Red ons uit het gevaar',
     link: '/2'
   },
-  '2021-11-29T00:00:00': {
+  '2022-11-29T00:00:00': {
     title: '3: DINSDAG NA DE 1e ZONDAG - God geeft uitkomst',
     link: '/3'
   },
-  '2021-11-30T00:00:00': {
+  '2022-11-30T00:00:00': {
     title: '4: WOENSDAG NA DE 1e ZONDAG - Wat vraagt God van mij?',
     link: '/4'
   },
-  '2021-12-1T00:00:00': {
+  '2022-12-1T00:00:00': {
     title: '5: DONDERDAG NA DE 1e ZONDAG - Zijt Gij het?',
     link: '/5'
   },
-  '2021-12-2T00:00:00': {
+  '2022-12-2T00:00:00': {
     title: '6: VRIJDAG NA DE 1e ZONDAG - Opstaan',
     link: '/6'
   },
-  '2021-12-3T00:00:00': {
+  '2022-12-3T00:00:00': {
     title: '7: ZATERDAG NA DE 1e ZONDAG - De komst van de vredevorst',
     link: '/7'
   },
-  '2021-12-4T00:00:00': {
+  '2022-12-4T00:00:00': {
     title: '8: 2e ZONDAG VAN DE ADVENT - De vreugde is op komst',
     link: '/8'
   }
