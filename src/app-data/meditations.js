f73fd900ff90c154be00452d2616fbb0a226e07d
Marcin Czenko <marcin.czenko@pm.me>
2022-12-05 04:04:03 +0100

    corrects year

--- src/app-data/meditations.js
+++ src/app-data/meditations.js
@@ -35,16 +35,16 @@ export const meditations = {
     title: '8: 2e ZONDAG VAN DE ADVENT - De vreugde is op komst',
     link: '/8'
   },
-  '2021-12-5T00:00:00': {
+  '2022-12-5T00:00:00': {
     title:
       '9: MAANDAG NA DE 2e ZONDAG - Het aardse verachten in liefde voor het hemelse',
     link: '/9'
   },
-  '2021-12-6T00:00:00': {
+  '2022-12-6T00:00:00': {
     title: '10: DINSDAG NA DE 2e ZONDAG - Zijn stem',
     link: '/10'
   },
-  '2021-12-7T00:00:00': {
+  '2022-12-7T00:00:00': {
     title: '389: 8 DECEMBER - De onbevlekte Ontvangenis',
     link: '/389',
     extras: [
@@ -55,7 +55,7 @@ export const meditations = {
       }
     ]
   },
-  '2021-12-8T00:00:00': {
+  '2022-12-8T00:00:00': {
     title: '389: 8 DECEMBER - De onbevlekte Ontvangenis',
     link: '/389',
     extras: [
@@ -66,15 +66,15 @@ export const meditations = {
       }
     ]
   },
-  '2021-12-9T00:00:00': {
+  '2022-12-9T00:00:00': {
     title: '13: VRIJDAG NA DE 2e ZONDAG - Vrede voor allen',
     link: '/13'
   },
-  '2021-12-10T00:00:00': {
+  '2022-12-10T00:00:00': {
     title: '14: ZATERDAG NA DE 2e ZONDAG - God beminnen',
     link: '/14'
   },
-  '2021-12-11T00:00:00': {
+  '2022-12-11T00:00:00': {
     title: '15: 3e ZONDAG VAN DE ADVENT - Christelijke vreugde',
     link: '/15'
   }
