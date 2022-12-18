b874ed71d47987dfb175921f103bf23b0fa0df03
Marcin Czenko <marcin.czenko@pm.me>
2022-12-18 23:29:56 +0100

    updates index.js from mdx components

--- src/components/mdx/index.js
+++ src/components/mdx/index.js
@@ -1,6 +1,3 @@
-import ImportingMDXDocuments from './ImportingMDXDocuments.mdx'
-export { WithBorder } from './WithBorder'
 export { BlogImage } from './BlogImage'
-export { ImportingMDXDocuments }
 export { MeditationForToday } from './MeditationForToday'
 export { CenteredText } from './CenteredText'
